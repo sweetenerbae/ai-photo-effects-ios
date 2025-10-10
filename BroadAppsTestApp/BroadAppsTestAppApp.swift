f144fa74063f6d97ce55fa40320e8aa8256c8fa7
sweetenerbae <diana444435@gmail.com>
2025-10-10 15:58:37 +0300

    Initial Commit

---
+++ BroadAppsTestApp/BroadAppsTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BroadAppsTestAppApp.swift
+//  BroadAppsTestApp
+//
+//  Created by Diana Kuchaeva on 10.10.25.
+//
+
+import SwiftUI
+
+@main
+struct BroadAppsTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
